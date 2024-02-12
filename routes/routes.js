bfe187c7159ee6dff946dac3995a022e928e3ff3
shivam singh <ssultanpura9@gmail.com>
2024-02-12 08:47:41 -0800

    delete todo routes added

--- routes/routes.js
+++ routes/routes.js
@@ -2,12 +2,31 @@ const express = require("express");
 const todoList = require("../models/todo");
 const routes = express.Router();
 routes.post("/addTodo", async (req, res) => {
-    console.log(req.body)
-   
+  console.log(req.body);
+try{
   const savedResult = await todoList.create({
     title: req.body.title,
     data: req.body.data,
   });
-  res.status(200).json(savedResult)
+  return res.status(200).json(savedResult);  
+}catch(err){
+    return res.status(400).json("todo not added..try again")
+}
+  
+});
+routes.post("/deleteTodo", async (req, res) => {
+  const delId = req.query.delId;
+  console.log(delId)
+  
+  try{
+      const todoFind = await todoList.findOne({ where: { id: delId } });
+  const deletedTodo=await todoFind.destroy()
+  console.log(deletedTodo)
+  return res.status(200).json({msg:"todo deleted",deletedTodo})
+  }
+  catch(err){
+    return res.status(400).json("todo not deleted..try again")
+}
+
 });
 module.exports = routes;
